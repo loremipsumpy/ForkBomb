daa56f98b2b28a0cc19f617e60da5a57cbba7774
loremipsumpy <adrian.py93@gmail.com>
2016-06-02 12:24:30 -0400

    test 2

--- app/src/main/java/dfinetec/lorem/com/forkbomb/MainActivity.java
+++ app/src/main/java/dfinetec/lorem/com/forkbomb/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
                     counter = 1;
                     execCommands(commands);
 
-
+                    //
                 }
             });
         }
